2d1e6375949d7f7ba11519153cf0891846b9e540
jayjin <jayjinjay@gmail.com>
2017-02-23 11:24:52 +0900

    #2 category hover effect

--- client/modules/Post/components/Input.js
+++ client/modules/Post/components/Input.js
@@ -1,15 +1,25 @@
 import React from 'react';
+import { Button, Popup, Grid } from 'semantic-ui-react';
 
 export default () => {
   return (
     <div className="ui huge fluid action input">
+      <Popup trigger={<Button>CTG</Button>} flowing hoverable>
+        <Grid centered divided columns={3}>
+          <Grid.Column textAlign="center">
+            <Button>Work</Button>
+          </Grid.Column>
+          <Grid.Column textAlign="center">
+            <Button>Play</Button>
+          </Grid.Column>
+          <Grid.Column textAlign="center">
+            <Button>Etc</Button>
+          </Grid.Column>
+        </Grid>
+      </Popup>
       <input type="text" placeholder="I want to..." />
-      <button className="ui yellow icon button">
-        Today
-      </button>
-      <button className="ui teal icon button">
-        Later
-      </button>
+      <button className="ui yellow icon button">Today</button>
+      <button className="ui teal icon button">Later</button>
     </div>
   );
 };
